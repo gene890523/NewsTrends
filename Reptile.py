ffafddd2eaec8b42db574fc5d52db3f36541e6dd
王博君 <gene@wangbojunde-MacBook-Pro.local>
2020-05-11 02:50:13 +0800

    add_delay_time

--- Reptile.py
+++ Reptile.py
@@ -13,36 +13,46 @@ from requests.adapters import HTTPAdapter
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-GOOGLE_TREND_API = 'https://trends.google.com.tw/trends/api/dailytrends?hl=zh-TW&tz=-480&geo=TW&ns=15'
+GOOGLE_TREND_API = 'https://trends.google.com.tw/trends/api/dailytrends?hl=zh-TW&tz=-480&ed={}&geo=TW&ns=15'
 
-response = requests.get(GOOGLE_TREND_API)
+# https://trends.google.com.tw/trends/api/dailytrends?hl=zh-TW&tz=-480&ed=20200507&geo=TW&ns=15
 
-Tmp = response.text
+def renew(date):
+    response = requests.get(GOOGLE_TREND_API.format(date))
+    Tmp = response.text
 
-Tmp = json.loads(Tmp.replace(')]}\',\n', ''))
+    Tmp = json.loads(Tmp.replace(')]}\',\n', ''))
 
-data = Tmp['default']['trendingSearchesDays'][0]['trendingSearches']
-print 'data length: {}'.format(len(data))
+    data = Tmp['default']['trendingSearchesDays'][0]['trendingSearches']
+    print 'data length: {}'.format(len(data))
 
-imageList = []
+    for i in range(0, len(data)):
+    # for i in range(0, 1)
+        firstArticle = data[i]['articles'][0]
+        print firstArticle['url']
+        print firstArticle['title']
+        firstUrl = firstArticle['url']
+        
+        response = requests.get(firstUrl)
+        # print 'success'
+        soup = BeautifulSoup(response.text, 'lxml')
+        imgUrl = soup.find("meta", property="og:image")['content']
+        print imgUrl
+        imageList.append({'img':imgUrl, 'url':firstUrl, 'title':firstArticle['title']})
 
-for i in range(0, len(data)):
-# for i in range(0, 1):
-    firstArticle = data[i]['articles'][1]
-    print firstArticle['url']
-    print firstArticle['title']
-    firstUrl = firstArticle['url']
-    
-    response = requests.get(firstUrl)
-    print 'success'
-    soup = BeautifulSoup(response.text, 'lxml')
-    imgUrl = soup.find("meta", property="og:image")['content']
-    print imgUrl
-    imageList.append({'img':imgUrl, 'url':firstUrl, 'title':firstArticle['title']})
+    # print imageList
 
-print imageList
+while 1:
+    imageList = []
+    for i in range( 0, 3 ):
+        date = (datetime.now() - timedelta(i)).strftime('%Y%m%d')
+        renew(date)
 
-jsString = 'var imageList = {};'.format(json.dumps(imageList))
+    jsString = 'var imageList = {};'.format(json.dumps(imageList))
 
-with open( 'data.js', "w" ) as text_file:
-    text_file.write(jsString)
+    with open( 'data.js', "w" ) as text_file:
+        text_file.write(jsString)
+
+    print 'end'
+
+    time.sleep(3600)
